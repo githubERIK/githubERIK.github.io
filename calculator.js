f290224a7cdc82423345c87d7512501a67b16dd7
githubERIK <githubERIK@users.noreply.github.com>
2026-02-14 19:49:43 +0200

    .

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById('contentCreationBtn').addEventListener('click', (event) => openTab(event, 'contentCreation'));
+document.getElementById('socialMediaManagementBtn').addEventListener('click', (event) => openTab(event, 'socialMediaManagement'));
+
 const locPrices = { "Tartu": 0, "Võru": 142, "Tallinn": 372 };
 
 const videos = document.getElementById('videos');
